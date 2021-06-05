763fcb7eba777a069c87e839ec04cf9900dfe9a5
Samuel Roth <samuel6roth@gmail.com>
2021-06-05 12:27:48 -0500

    deploying again

--- server.js
+++ server.js
@@ -14,11 +14,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workoutTracker',
   {
