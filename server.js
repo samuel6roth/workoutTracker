17f59cb5422d460a8c9259df499c6ad0dd65f248
Samuel Roth <samuel6roth@gmail.com>
2021-06-05 19:07:27 -0500

    Fixing file names, app error

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(
@@ -25,7 +22,7 @@ mongoose.connect(
 );
 
 app.use(require("./routes/api.js"));
-app.use(require("./routes/htmlroutes"));
+app.use(require("./routes/htmlroutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
